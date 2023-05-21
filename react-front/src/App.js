20c396fa298fd9beee74d378e1b41cc58757aa17
Your Name <you@example.com>
2023-05-21 20:03:52 +0200

    first committtt

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [file, setFile] = useState(null);
+  const [prediction, setPrediction] = useState('');
+  const [error, setError] = useState('');
+
+  const handleFileUpload = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+      setError('');
+    } else {
+      setError('No file selected');
+    }
+  };
+  
+
+  const handlePredict = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data' // Set the Content-Type header to 'application/json'
+        }
+      };
+
+      const response = await axios.post('http://localhost:5000/predict', formData, config);
+
+      if (response.data.error) {
+        setError(response.data.error);
+        setPrediction('');
+      } else {
+        const predictions = response.data.predictions;
+        setPrediction(predictions);
+        setError('');
+      }
+    } catch (error) {
+      console.error(error);
+      setError('An error occurred. Please try again.');
+      setPrediction('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" accept=".json" onChange={handleFileUpload} />
+      <button onClick={handlePredict}>Predict</button>
+      {error && <p>Error: {error}</p>}
+      {prediction && !error && <p>Prediction: {prediction}</p>}
     </div>
   );
-}
+};
 
 export default App;

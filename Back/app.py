84204e7db5be403da6a1c61679eebedbc4339298
Your Name <you@example.com>
2023-06-02 23:44:03 +0200

    One Interface in DONE

--- Back/app.py
+++ Back/app.py
@@ -6,7 +6,8 @@ from sklearn.preprocessing import MinMaxScaler
 from flask_cors import CORS
 
 app = Flask(__name__)
-model = load('sample.joblib')
+model = load('MyModal1.joblib')
+selector, selected_features = load('MyFeatureSelection1.joblib')
 CORS(app)
 
 @app.route('/predict', methods=['POST'])
@@ -16,21 +17,18 @@ def predict():
         if file.filename.endswith('.json'):
             file_content = file.read()
             df = pd.read_json(io.BytesIO(file_content))
-            print(df)
-
-            # Preprocess the JSON data
             
-            df.columns = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land', 'wrong_fragment',
-                          'urgent', 'hot', 'num_failed_logins', 'logged_in', 'num_compromised', 'root_shell', 'su_attempted',
-                          'num_root', 'num_file_creations', 'num_shells', 'num_access_files', 'num_outbound_cmds',
-                          'is_host_login', 'is_guest_login', 'count', 'srv_count', 'serror_rate', 'srv_serror_rate',
-                          'rerror_rate', 'srv_rerror_rate', 'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate',
-                          'dst_host_count', 'dst_host_srv_count', 'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',
+            # Preprocess the JSON data
+            df.columns = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land',
+                          'wrong_fragment', 'urgent', 'hot', 'num_failed_logins', 'logged_in', 'num_compromised',
+                          'root_shell', 'su_attempted', 'num_root', 'num_file_creations', 'num_shells',
+                          'num_access_files', 'num_outbound_cmds', 'is_host_login', 'is_guest_login', 'count',
+                          'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate',
+                          'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate', 'dst_host_count',
+                          'dst_host_srv_count', 'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',
                           'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate',
                           'dst_host_srv_serror_rate', 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate']
-
-           
-
+            
             df['protocol_type'].replace({'udp': 0, 'tcp': 1, 'icmp': 2}, inplace=True)
 
             df['flag'].replace({'OTH': 0, 'REJ': 1, 'RSTO': 2, 'RSTOS0': 3, 'RSTR': 4, 'S0': 5, 'S1': 6, 'S2': 7,
@@ -53,10 +51,20 @@ def predict():
             scaler = MinMaxScaler()
             df_scaled = scaler.fit_transform(df.iloc[:, :-1])
             df_scaled = pd.DataFrame(df_scaled, columns=df.columns[:-1])
+
+
+            # Check if all the selected features are present in the prediction data 
+            missing_features = set(selected_features) - set(df.columns)
+            if missing_features:
+             return jsonify({'error': f'Missing required features: {", ".join(missing_features)}'})
+
+            
+           # Apply feature selection to the prediction data
+            df_selected = pd.DataFrame(df_scaled, columns=df.columns[:-1])[selected_features]
             
             # Make predictions using the loaded model
-            predictions = model.predict(df_scaled)
-            labels = ['NonDoS' if pred == 0 else 'DoS' for pred in predictions]
+            predictions = model.predict(df_selected)
+            labels = ['NonDoS   ' if pred == 0 else 'DoS   ' for pred in predictions]
 
             response_data = {"predictions": labels}
             return jsonify(response_data)
@@ -65,6 +73,78 @@ def predict():
     except Exception as e:
         error_message = str(e)
         return jsonify({'error': error_message})
+
+
+####Another Route
+@app.route('/predict-Unsw', methods=['POST'])
+def predictUnsw():
+    try:
+        file1 = request.files['file1']
+        if file1.filename.endswith('.json'):
+            file_content = file1.read()
+            df = pd.read_json(io.BytesIO(file_content))
+            
+            # Preprocess the JSON data
+            df.columns = ['duration', 'protocol_type', 'service', 'flag', 'src_bytes', 'dst_bytes', 'land',
+                          'wrong_fragment', 'urgent', 'hot', 'num_failed_logins', 'logged_in', 'num_compromised',
+                          'root_shell', 'su_attempted', 'num_root', 'num_file_creations', 'num_shells',
+                          'num_access_files', 'num_outbound_cmds', 'is_host_login', 'is_guest_login', 'count',
+                          'srv_count', 'serror_rate', 'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate',
+                          'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate', 'dst_host_count',
+                          'dst_host_srv_count', 'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',
+                          'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate', 'dst_host_serror_rate',
+                          'dst_host_srv_serror_rate', 'dst_host_rerror_rate', 'dst_host_srv_rerror_rate']
+            
+            df['protocol_type'].replace({'udp': 0, 'tcp': 1, 'icmp': 2}, inplace=True)
+
+            df['flag'].replace({'OTH': 0, 'REJ': 1, 'RSTO': 2, 'RSTOS0': 3, 'RSTR': 4, 'S0': 5, 'S1': 6, 'S2': 7,
+                                'S3': 8, 'SF': 9, 'SH': 10}, inplace=True)
+
+            df['service'].replace({'aol': 0, 'auth': 1, 'bgp': 2, 'courier': 3, 'csnet_ns': 4, 'ctf': 5, 'daytime': 6,
+                                   'discard': 7, 'domain': 8, 'domain_u': 9, 'echo': 10, 'eco_i': 11, 'ecr_i': 12,
+                                   'efs': 13, 'exec': 14, 'finger': 15, 'ftp': 16, 'ftp_data': 17, 'gopher': 18,
+                                   'harvest': 19, 'hostnames': 20, 'http': 21, 'http_2784': 22, 'http_443': 23,
+                                   'http_8001': 24, 'imap4': 25, 'IRC': 26, 'iso_tsap': 27, 'klogin': 28, 'kshell': 29,
+                                   'ldap': 30, 'link': 31, 'login': 32, 'mtp': 33, 'name': 34, 'netbios_dgm': 35,
+                                   'netbios_ns': 36, 'netbios_ssn': 37, 'netstat': 38, 'nnsp': 39, 'nntp': 40,
+                                   'ntp_u': 41, 'other': 42, 'pm_dump': 43, 'pop_2': 44, 'pop_3': 45, 'printer': 46,
+                                   'private': 47, 'red_i': 48, 'remote_job': 49, 'rje': 50, 'shell': 51, 'smtp': 52,
+                                   'sql_net': 53, 'ssh': 54, 'sunrpc': 55, 'supdup': 56, 'systat': 57, 'telnet': 58,
+                                   'tftp_u': 59, 'tim_i': 60, 'time': 61, 'urh_i': 62, 'urp_i': 63, 'uucp': 64,
+                                   'uucp_path': 65, 'vmnet': 66, 'whois': 67, 'X11': 68, 'Z39_50': 69}, inplace=True)
+
+            # Scaling
+            scaler = MinMaxScaler()
+            df_scaled = scaler.fit_transform(df.iloc[:, :-1])
+            df_scaled = pd.DataFrame(df_scaled, columns=df.columns[:-1])
+
+
+            # Check if all the selected features are present in the prediction data 
+            missing_features = set(selected_features) - set(df.columns)
+            if missing_features:
+             return jsonify({'error': f'Missing required features: {", ".join(missing_features)}'})
+
+            
+           # Apply feature selection to the prediction data
+            df_selected = pd.DataFrame(df_scaled, columns=df.columns[:-1])[selected_features]
+            
+            # Make predictions using the loaded model
+            predictions = model.predict(df_selected)
+            labels = ['NonDoS   ' if pred == 0 else 'DoS   ' for pred in predictions]
+
+            response_data = {"predictions": labels}
+            return jsonify(response_data)
+        else:
+            return jsonify({'error': 'Invalid file format. Please upload a JSON file.'})
+    except Exception as e:
+        error_message = str(e)
+        return jsonify({'error': error_message})
+
+
+
+
+####End
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,21 +1,46 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+import './App.css';
+
 const App = () => {
   const [file, setFile] = useState(null);
   const [prediction, setPrediction] = useState('');
+  const [fileName, setFileName] = useState('');
   const [error, setError] = useState('');
+
+  //////for second
+
+  const [file1, setFile1] = useState(null);
+  const [prediction1, setPrediction1] = useState('');
+  const [fileName1, setFileName1] = useState('');
+  const [error1, setError1] = useState('');
+
 
   const handleFileUpload = (event) => {
     const selectedFile = event.target.files[0];
     if (selectedFile) {
       setFile(selectedFile);
+      setFileName(selectedFile.name); // Set the selected file name
       setError('');
     } else {
       setError('No file selected');
     }
   };
-  
+
+  ///////file of Unsw
+   
+  const handleFileUploadUnsw = (event) => {
+    const selectedFile1 = event.target.files[0];
+    if (selectedFile1) {
+      setFile1(selectedFile1);
+      setFileName1(selectedFile1.name); // Set the selected file name
+      setError1('');
+    } else {
+      setError1('No file selected');
+    }
+  };
+  //////end of file unsw 
 
   const handlePredict = async () => {
     try {
@@ -44,13 +69,119 @@ const App = () => {
       setPrediction('');
     }
   };
+///////////////Predictother
+
+const handlePredictUnsw = async () => {
+  try {
+    const formData1 = new FormData();
+    formData1.append('file1', file1);
+
+    const config1 = {
+      headers: {
+        'Content-Type': 'multipart/form-data' // Set the Content-Type header to 'application/json'
+      }
+    };
+
+    const response1 = await axios.post('http://localhost:5000/predict-Unsw', formData1, config1);
+
+    if (response1.data.error) {
+      setError1(response1.data.error);
+      setPrediction1('');
+    } else {
+      const predictions1 = response1.data.predictions;
+      setPrediction1(predictions1);
+      setError1('');
+    }
+  } catch (error1) {
+    console.error(error1);
+    setError1('An error occurred. Please try again.');
+    setPrediction1('');
+  }
+};
+//////end handle other predict
+
 
   return (
-    <div>
-      <input type="file" accept=".json" onChange={handleFileUpload} />
-      <button onClick={handlePredict}>Predict</button>
-      {error && <p>Error: {error}</p>}
-      {prediction && !error && <p>Prediction: {prediction}</p>}
+    <div className='containers'>
+     
+      <div className='container-NSL' >
+       <h1>NSL-KDD</h1>
+
+      <div className='container-prediction'>
+        
+     {/* <input className='NSL-Input' type="file" accept=".json" onChange={handleFileUpload} />  */}
+     <label className="label">
+   <input type="file"  accept=".json" onChange={handleFileUpload} />
+   <span>{fileName || 'Select a file'}</span>
+  </label>
+
+
+      <button className='button-17' onClick={handlePredict}>Predict</button>
+      
+      {prediction.length > 0 && !error && (
+        <table className='prediction-table'>
+          <thead>
+            <tr>
+              <th>Index</th>
+              <th>Prediction</th>
+            </tr>
+          </thead>
+          <tbody>
+            {prediction.map((pred, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{pred}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+            {error && <p>Error: {error}</p>}
+
+      </div>
+      </div>
+
+      <div className='container-UNSW' >
+       <h1> UNSW_NB15</h1>
+
+      <div className='container-prediction'>
+        
+     
+     <label className="label">
+   <input type="file"  accept=".json" onChange={handleFileUploadUnsw} />
+   <span>{fileName1 || 'Select a file'}</span>
+  </label>
+
+
+      <button className='button-17' onClick={handlePredictUnsw}>Predict</button>
+      
+      {prediction1.length > 0 && !error1 && (
+        <table className='prediction-table'>
+          <thead>
+            <tr>
+              <th>Index</th>
+              <th>Prediction</th>
+            </tr>
+          </thead>
+          <tbody>
+            {prediction1.map((pred, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{pred}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
+      {error1 && <p>Error: {error1}</p>}
+
+      </div>
+      </div>
+ 
+      
+
+      
     </div>
   );
 };

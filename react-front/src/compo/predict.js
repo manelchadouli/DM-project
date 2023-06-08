5eced11f5b48f53856af3be4674e1bdd747d48c3
Abir_Ba <a.baiai@esi-sba.dz>
2023-06-08 23:35:34 +0100

    UNSW_NB15 predict

--- react-front/src/compo/predict.js
+++ react-front/src/compo/predict.js
@@ -31,16 +31,16 @@ const Predict = ()=>{
   
     ///////file of Unsw
      
-    // const handleFileUploadUnsw = (event) => {
-    //   const selectedFile1 = event.target.files[0];
-    //   if (selectedFile1) {
-    //     setFile1(selectedFile1);
-    //     setFileName1(selectedFile1.name); // Set the selected file name
-    //     setError1('');
-    //   } else {
-    //     setError1('No file selected');
-    //   }
-    // };
+    const handleFileUploadUnsw = (event) => {
+      const selectedFile1 = event.target.files[0];
+      if (selectedFile1) {
+        setFile1(selectedFile1);
+        setFileName1(selectedFile1.name); // Set the selected file name
+        setError1('');
+      } else {
+        setError1('No file selected');
+      }
+    };
 
     
     //////end of file unsw 
@@ -83,37 +83,42 @@ const Predict = ()=>{
         setPredictions('');
       }
     };
-  ///////////////Predict UNSW
-  
-  // const handlePredictUnsw = async () => {
-  //   try {
-  //     const formData1 = new FormData();
-  //     formData1.append('file1', file1);
-  
-  //     const config1 = {
-  //       headers: {
-  //         'Content-Type': 'multipart/form-data' // Set the Content-Type header to 'application/json'
-  //       }
-  //     };
-  
-  //     const response1 = await axios.post('http://localhost:5000/predict-Unsw', formData1, config1);
-  
-  //     if (response1.data.error) {
-  //       setError1(response1.data.error);
-  //       setPrediction1('');
-  //     } else {
-  //       const predictions1 = response1.data.predictions;
-  //       setPrediction1(predictions1);
-  //       setError1('');
-  //     }
-  //   } catch (error1) {
-  //     console.error(error1);
-  //     setError1('An error occurred. Please try again.');
-  //     setPrediction1('');
-  //   }
-  // };
-  //////end handle UNSW predict
-  
+ ///////////////Predict UNSW
+
+const handlePredictUnsw = async () => {
+  try {
+    const formData1 = new FormData();
+    formData1.append('file1', file1);
+
+    const config1 = {
+      headers: {
+        'Content-Type': 'multipart/form-data' // Set the Content-Type header to 'application/json'
+      }
+    };
+
+    const response1 = await axios.post('http://localhost:5000/predict-Unsw', formData1, config1);
+
+    if (response1.data.error) {
+      setError1(response1.data.error);
+      setPrediction1('');
+    } else {
+      const { data4 ,data5 ,data6,data7} = response1.data;
+          const prediction1 = {
+            predictions1: data4.predictions4,
+            predictions2: data5.predictions5,
+            predictions3: data6.predictions6,
+            predictions4: data7.predictions7,
+          };
+          setPrediction1(prediction1);
+          setError1('');
+    }
+  } catch (error1) {
+    console.error(error1);
+    setError1('An error occurred. Please try again.');
+    setPrediction1('');
+  }
+};
+//////end handle UNSW predict
   
 
     return(
@@ -177,13 +182,12 @@ const Predict = ()=>{
 
       </div>
       </div>
-
-       {/* <div className='container-UNSW' >
-       <h1> UNSW_NB15</h1>
+      <div className='container-UNSW' >
+       <h1>UNSW-NB15</h1>
 
       <div className='container-prediction'>
         
-     
+     {/* <input className='NSL-Input' type="file" accept=".json" onChange={handleFileUpload} />  */}
      <label className="label">
    <input type="file"  accept=".json" onChange={handleFileUploadUnsw} />
    <span>{fileName1 || 'Select a file'}</span>
@@ -191,31 +195,58 @@ const Predict = ()=>{
 
 
       <button className='button-17' onClick={handlePredictUnsw}>Predict</button>
-      
-      {prediction1.length > 0 && !error1 && (
+      {prediction1 && (
+        <div className='prediction-tableee'>
         <table className='prediction-table'>
-          <thead>
-            <tr>
-              <th>Index</th>
-              <th>Prediction</th>
-            </tr>
-          </thead>
-          <tbody>
-            {prediction1.map((pred, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{pred}</td>
+        <thead>
+          <tr>
+            <th>Algorithme de feature selection</th>
+            <th>Algorithme de Classification</th>
+
+            <th>Prediction</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+           
+              <tr >
+                <td>CHI2</td>
+                <td>KNN</td>
+                <td>  {prediction1.predictions1.map((prediction, index) => (
+               <li key={index}>{prediction} </li>
+            ))}</td>
               </tr>
-            ))}
+              <tr >
+                <td>Random Forest Selector</td>
+                <td>KNN</td>
+                <td>  {prediction1.predictions2.map((prediction, index) => (
+               <li key={index}>{prediction} </li>
+            ))}</td>
+              </tr>
+              <tr >
+                <td>Mutual Info</td>
+                <td>KNN</td>
+                <td>  {prediction1.predictions3.map((prediction, index) => (
+               <li key={index}>{prediction} </li>
+            ))}</td>
+              </tr>
+              <tr >
+                <td>RFE</td>
+                <td>KNN</td>
+                <td>  {prediction1.predictions4.map((prediction, index) => (
+               <li key={index}>{prediction} </li>
+            ))}</td>
+              </tr>
+            
           </tbody>
         </table>
-      )}
-
-      {error1 && <p>Error: {error1}</p>}
+        </div>
+        )}
+        
+            {error && <p>Error: {error}</p>}
 
       </div>
-      </div> 
-  */}
+      </div>
       
 
       
